29b228a5d577e4da114d997a17a312d07ad88a80
huangzhenqiang <huangzhenqiang@sogou-inc.com>
2017-08-16 23:32:54 +0800

    commit project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"beeblog/controllers"
+	"beeblog/models"
+	//_ "beeblog/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	models.RegisterDB()
+}
+
+func main() {
+	// 开启ORM 调试模式
+	orm.Debug = true
+	// 自动创建表
+	orm.RunSyncdb("default", false, true)
+
+	// 注册路由
+	beego.Router("/", &controllers.HomeController{})
+	beego.Router("/login", &controllers.LoginController{})
+
+	// 启动beego
+	beego.Run()
+}
---
+++ views/T.header.tpl
@@ -0,0 +1,11 @@
+{{define "header"}}
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <link rel="shortcut icon" href="/static/img/favicon.png">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
+    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="/static/js/bootstrap.min.js"></script>
+{{end}}
